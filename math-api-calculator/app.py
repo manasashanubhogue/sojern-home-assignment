c878ce3a743fe5ba123ee07935475d9c19724d95
Manasa Maravanthe <m.manasa21@gmail.com>
2023-06-18 13:26:43 +0000

    math api calculator functions & test cases

---
+++ math-api-calculator/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify
+import numpy as np
+
+app = Flask(__name__)
+
+def get_request_numbers_quantifiers():
+    data = request.get_json()
+    numbers = data.get('numbers', [])
+    quantifier = data.get('quantifier')
+   
+    if not numbers or not quantifier:
+        return jsonify({'error': 'Invalid request parameters'})
+
+    return numbers, quantifier
+
+@app.route('/min', methods=['POST'])
+def min_number():
+    numbers, quantifier = get_request_numbers_quantifiers()
+
+    numbers.sort()
+    min_numbers = numbers[:quantifier]
+    return jsonify({'min_numbers': min_numbers})
+
+@app.route('/max', methods=['POST'])
+def max_number():
+    numbers, quantifier = get_request_numbers_quantifiers()
+
+    numbers.sort(reverse=True)
+    max_numbers = numbers[:quantifier]
+    return jsonify({'max_numbers': max_numbers})
+
+@app.route('/avg', methods=['POST'])
+def average():
+    data = request.get_json()
+    numbers = data.get('numbers', [])
+
+    if not numbers:
+        return jsonify({'error': 'Invalid request parameters'})
+
+    avg = np.mean(numbers)
+    return jsonify({'average': avg})
+
+
+@app.route("/median", methods=["POST"])
+def calculate_median():
+    data = request.get_json()
+    numbers = data.get("numbers", [])
+    
+    median = np.median(numbers)
+
+    return jsonify({"median": median})
+
+@app.route("/percentile", methods=["POST"])
+def calculate_percentile():
+    numbers, quantifier = get_request_numbers_quantifiers()
+
+    sorted_numbers = sorted(numbers)
+    # p/100*N 
+    index = int(np.ceil((quantifier / 100) * len(sorted_numbers))) - 1
+    result = sorted_numbers[index]
+
+    return jsonify({"percentile": result})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ math-api-calculator/test_app.py
@@ -0,0 +1,58 @@
+import unittest
+import json
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_calculate_min(self):
+        payload = {
+            "numbers": [4, 2, 6, 1, 9],
+            "quantifier": 2
+        }
+        response = self.app.post("/min", json=payload)
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["min_numbers"], [1, 2])
+
+    def test_calculate_max(self):
+        payload = {
+            "numbers": [4, 2, 6, 1, 9],
+            "quantifier": 2
+        }
+        response = self.app.post("/max", json=payload)
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["max_numbers"], [9, 6])
+
+    def test_calculate_average(self):
+        payload = {
+            "numbers": [4, 2, 6, 1, 9]
+        }
+        response = self.app.post("/avg", json=payload)
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["average"], 4.4)
+
+    def test_calculate_median(self):
+        payload = {
+            "numbers": [4, 2, 6, 1, 9]
+        }
+        response = self.app.post("/median", json=payload)
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["median"], 4)
+
+    def test_calculate_percentile(self):
+        payload = {
+            "numbers": [4, 2, 6, 1, 9],
+            "quantifier": 75
+        }
+        response = self.app.post("/percentile", json=payload)
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["percentile"], 6)
+
+if __name__ == "__main__":
+    unittest.main()
